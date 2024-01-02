4b880ec834484de25f3b7c30931c37c21a050344
Daniel <dmartindiaz3@gmail.com>
2024-01-02 11:24:45 +0100

    Add port

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 
@@ -27,6 +27,12 @@ app.get('/updatematch', UpdateTournametMatch)
 app.get('/updateparticipant', UpdateParticipant)
 app.get('/updatestagename', UpdateParticipant)
 app.get('/delete', DeleteStage)
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Im working."
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
